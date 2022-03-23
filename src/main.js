6a7c9f0a132098ce6a0b0c8d93b48b90f5871cca
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2022-03-22 23:42:58 -0500

    Tablas diseño y filtros

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import  { filterSport, filterPais, sortData , computeStats }  from './data.js';
+import  { filterSport, filterPais,filterMedals, sortData, filterG  }  from './data.js';
 import gameData from './data/athletes/athletes.js';
 
 const athletes = gameData.athletes;
@@ -8,7 +8,7 @@ enterBtn.addEventListener('click',nombreFn)
 function nombreFn () {
     document.getElementById('bienvenida').style.display='none';
     document.getElementById('datos').style.display='block';  
-};
+}
 
 const tableBody = document.querySelector('tbody')
 const allAthletes = (index)=>{
@@ -18,6 +18,7 @@ const allAthletes = (index)=>{
     <td>${index.team}</td>
     <td>${index.sport}</td>
     <td>${index.medal}</td>
+    <td>${index.gender}</td>
     </tr>
     `;
 }
@@ -41,17 +42,39 @@ str2.addEventListener('change',(x)=>{
     printScreen(selectPais)
 })
 
+const str3=document.getElementById('medals');
+str3.addEventListener('change',(x)=>{
+    const selectMedals=filterMedals(athletes,x.target.value);
+    printScreen(selectMedals)
+})
 
+
+/* const str5=document.getElementById("Gender");
+str5.addEventListener('change',(x)=>{
+    const selectGenderF=filterF(athletes,x.target.value);
+    printScreen(selectGenderF)
+}) */
+
+/* const str6=document.getElementById("Gender");
+str6.addEventListener('change',(x)=>{
+    const selectGenderM=filterM(athletes,x.target.value);
+    printScreen(selectGenderM) 
+})*/
+
+const str4=document.getElementById('Ordenador');
+str4.addEventListener('change',(x)=>{
+    const selectName=sortData(athletes,x.target.value);
+    printScreen(selectName)
+})
+
+const str5=document.getElementById('Gender');
+str5.addEventListener('change',(x)=>{
+    const selectG=filterG(athletes,x.target.value);
+    printScreen(selectG)
+})
 //console.log(data);
 //console.log(filterSport(athletes,"Archery"));
-
-
-
-
-
-
-
-
+//Efectos boton
 var animateButton = function(e) {
 
     e.preventDefault;
@@ -64,8 +87,6 @@ var animateButton = function(e) {
     },700);
   };
   
-
-
   var bubblyButtons = document.getElementsByClassName("inicio");
     for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
